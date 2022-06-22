784038ae01b09fef5cccb39bde00b2a1f9103edf
RodrigoGomesNascimento <rgomes.nascimento@gmail.com>
2022-06-22 15:23:05 -0300

    Inserindo Migrate e Models

--- resources/views/site/layouts/basico.blade.php
+++ resources/views/site/layouts/basico.blade.php
@@ -2,12 +2,14 @@
 <html lang="pt-br">
 
 <head>
-    <title>Super Gestão - Sobre Nós</title>
+    <title>Super Gestão - @yield ('titulo')</title>
     <meta charset="utf-8">
     <link rel="stylesheet" href="{{ asset('css/estilo_basico.css') }}">
 </head>
 
 <body>
+    {{-- cria o menu em todas a paginas que extend o basico. --}}
+    @include('site.layouts._partials.topo')
     {{-- PARA IMPRIMIR A SEÇÃO NO LOCAL CERTO. --}}
     @yield('conteudo')
 
